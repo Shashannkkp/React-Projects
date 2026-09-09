a69776907937063e553961ce28be37f2ab31c8c2
Ultmatepredator <patelshashank357@gmail.com>
2026-09-09 15:33:05 +0530

    new project added

---
+++ CurrencyConvertor/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+  <>
+  <h1 className='text-3xl bg-blue-500'> Currency app with chai Chai</h1>
+  
+  </>
+  )
+}
+
+export default App
