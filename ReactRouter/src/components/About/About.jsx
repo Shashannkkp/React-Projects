826fd1d5a7dc52f48671a18fb5af0d3c34edca95
Ultmatepredator <patelshashank357@gmail.com>
2026-09-18 00:47:59 +0530

    About section updated

--- ReactRouter/src/components/About/About.jsx
+++ ReactRouter/src/components/About/About.jsx
@@ -3,8 +3,9 @@ export default function About() {
         <div className="py-16 bg-white">
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                    <div className="md:5/12 lg:w-5/12">
+                    <div className="md:5/12 lg:w-5/12 rounded-x1">
                         <img
+                        className="rounded-xl"
                             src="https://efcindia.org/wp-content/uploads/2025/12/126638-ezgif.com-jpg-to-webp-converter.webp"
                             alt="image"
                         />
